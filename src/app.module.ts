604553f973c6716e2e62ecc1a4141211ae3ea628
ivan.sigaev <ivan.develop@gmail.com>
2024-11-12 00:16:43 +0100

    feat: export swagger, add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { FavoriteModule } from './favorite/favorite.module';
 
 @Module({
   imports: [
@@ -12,6 +15,9 @@ import { ArtistModule } from './artist/artist.module';
     }),
     UserModule,
     ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavoriteModule,
   ],
   controllers: [AppController],
   providers: [AppService],
